7ffeacd34e3ba57838a492229032b3e8f13f799e
D_water <chrisjerry070@gmail.com>
2023-11-14 01:04:24 +0400

    added destination page

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,33 +1,95 @@
-import Navbar from "../components/Navbar"
-import { Link } from "react-router-dom"
-
+import Navbar from "../components/Navbar";
+import data from "../data.json";
+import { MouseEventHandler, useState } from "react";
 
 function Destination() {
+  const [selectedObject, setSelectedObject] = useState<{
+    name: string;
+    images: { png: string; webp: string };
+    description: string;
+    distance: string;
+    travel: string;
+  }>(data.destinations[0]);
+  console.log(selectedObject.images.png);
+
+  const handleClick: MouseEventHandler<HTMLLIElement> = (event): void => {
+    const liElement = event.target as HTMLLIElement;
+    const className = liElement.className;
+
+    switch (className) {
+      case "moon":
+        setSelectedObject(data.destinations[0]);
+        break;
+      case "mars":
+        setSelectedObject(data.destinations[1]);
+        break;
+      case "europa":
+        setSelectedObject(data.destinations[2]);
+        break;
+      case "titan":
+        setSelectedObject(data.destinations[3]);
+        break;
+
+      default:
+        setSelectedObject(data.destinations[0]);
+        break;
+    }
+  };
   return (
-      <section className="destination">
-        <Navbar />
-        <main className="destination-box">
-          <p className="destination-header">Pick your destination</p>
-          <div className="destination-content">
-            <div className="destination-image">
-
-            </div>
-            <article className="destination-text">
-              <nav>
-                <ul>
-                  <li><Link to="/">moon</Link></li>
-                  <li><Link to="/">mars</Link></li>
-                  <li><Link to="/">europa</Link></li>
-                  <li><Link to="/">titan</Link></li>
-                </ul>
-              </nav>
-
-            </article>
+    <section className="destination">
+      <Navbar />
+      <main className="destination-box">
+        <p className="destination-header">
+          {" "}
+          <span>0.1</span> Pick your destination
+        </p>
+        <div className="destination-content">
+          <div className="destination-image">
+            <img src={selectedObject.images.webp} alt="destination image" />
           </div>
-        </main>
-        
-      </section>
-  )
+          <article className="destination-text">
+            <nav>
+              <ul>
+                {/* fix me add active classlist */}
+                <li className="moon" onClick={handleClick}>
+                  moon
+                </li>
+                <li className="mars" onClick={handleClick}>
+                  mars
+                </li>
+                <li className="europa" onClick={handleClick}>
+                  europa
+                </li>
+                <li className="titan" onClick={handleClick}>
+                  titan
+                </li>
+              </ul>
+            </nav>
+            <div className="destination-details">
+              <div className="destination-detail">
+                <h1>{selectedObject.name}</h1>
+                <div className="destination-descri">
+                  <p>{selectedObject.description}</p>
+                </div>
+                <div className="destination-time">
+                  <div>
+                    <p>avg. distance</p>
+                    <p className="destination-dist">
+                      {selectedObject.distance}
+                    </p>
+                  </div>
+                  <div>
+                    <p>est. travel time</p>
+                    <p className="destination-dist">{selectedObject.travel}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </div>
+      </main>
+    </section>
+  );
 }
 
-export default Destination
+export default Destination;
