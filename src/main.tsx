80c5b47128278b4b45ef237b37bb71fbbc07e304
D_water <chrisjerry070@gmail.com>
2023-11-19 14:10:50 +0400

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './sass/main.scss'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Author from './pages/Author'
-import Technology from './pages/Technology'
-import Destination from './pages/Destination'
-import Mistake from './pages/Mistake'
-import Home from './components/Home'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
